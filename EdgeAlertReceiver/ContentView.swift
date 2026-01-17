dc1b17e0edd58d29c202ce2c0e1d97bcceba9d92
Jonatas Santos <jonatas.dlp@gmail.com>
2026-01-17 18:43:09 -0300

    Initial Commit

---
+++ EdgeAlertReceiver/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  EdgeAlertReceiver
+//
+//  Created by Jonatas Santos on 17/01/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ EdgeAlertReceiver/EdgeAlertReceiverApp.swift
@@ -0,0 +1,17 @@
+//
+//  EdgeAlertReceiverApp.swift
+//  EdgeAlertReceiver
+//
+//  Created by Jonatas Santos on 17/01/26.
+//
+
+import SwiftUI
+
+@main
+struct EdgeAlertReceiverApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
